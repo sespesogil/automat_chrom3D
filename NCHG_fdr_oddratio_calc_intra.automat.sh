c62362d30062e493167ece330dbedee5efdbd59a
sespesogil <42008709+sespesogil@users.noreply.github.com>
2022-04-09 03:39:13 -0700

    Update NCHG_fdr_oddratio_calc_intra.automat.sh

--- NCHG_fdr_oddratio_calc_intra.automat.sh
+++ NCHG_fdr_oddratio_calc_intra.automat.sh
@@ -1,4 +1,3 @@
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 
@@ -8,9 +7,9 @@ stats=$2
 thresold=$3
 res=$4
 
-python NCHG_fdr_oddratio_calc.py $chrom3D_dir/intrachr_bedpe/sample.$res.domain.RAW.bedpe.out $stats 2 $thresold > $chrom3D_dir/intrachr_bedpe/sample.$res.domain.RAW.bedpe.sig
+input=$chrom3D_dir/intrachr_bedpe/sample.domain.RAW.bedpe.out
 
-
+python NCHG_fdr_oddratio_calc.py $input $stats 2 $thresold > $chrom3D_dir/intrachr_bedpe/sample.domain.RAW.bedpe.sig
 
 
 
